d8ded58b647ed11bdaa32fcc5acc46a9fe998925
Duang <duangkamol.ka@northbkk.ac.th>
2015-11-29 12:08:28 +0700

    Web View เสร์จแล้ว

---
+++ app/src/main/java/com/northbkk/drduang/mytraffic/MyNorth.java
@@ -0,0 +1,12 @@
+package com.northbkk.drduang.mytraffic;
+
+import android.content.Context;
+
+/**
+ * Created by kaewdaeng on 29/11/15.
+ */
+public class MyNorth {
+
+    //Explicit
+    private Context objContext;
+}//Main Class
